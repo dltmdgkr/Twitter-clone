63a7215eee39dc20826882ee27aa74d84aa80e16
dltmdgkr <shl907@naver.com>
2024-03-31 11:05:05 +0900

    /explore page, /search 검색필터

--- src/app/(afterLogin)/_component/TrendSection.tsx
+++ src/app/(afterLogin)/_component/TrendSection.tsx
@@ -1,22 +1,31 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import style from "./trendSection.module.css";
 import Trend from "@/app/(afterLogin)/_component/Trend";
 
 export default function TrendSection() {
+  const path = usePathname();
+
   return (
-    <div className={style.trendBg}>
-      <div className={style.trend}>
-        <h3>나를 위한 트렌드</h3>
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-        <Trend />
-      </div>
-    </div>
+    <>
+      {path === "/explore" ? null : (
+        <div className={style.trendBg}>
+          <div className={style.trend}>
+            <h3>나를 위한 트렌드</h3>
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+            <Trend />
+          </div>
+        </div>
+      )}
+    </>
   );
 }
